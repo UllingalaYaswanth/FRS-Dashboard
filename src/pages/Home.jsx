889dee84f1c077a84f97fb9a23b7c3af3e48be5b
ullingala yaswanth <yaswanthullingala@gmail.com>
2025-01-20 18:18:27 +0530

    updated login details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <div className='grid md:grid-cols-4 gap-4 p-4 py-6'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 py-6'>
       <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
         <h1 className='text-lg font-semibold'>Camera 1</h1>
       </div>
