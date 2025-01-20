ffd766746a294d72973cfc7244653e466062b8fc
ullingala yaswanth <yaswanthullingala@gmail.com>
2025-01-20 16:38:20 +0530

    cameras

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className='grid md:grid-cols-4 gap-4 p-4 py-6'>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 1</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 2</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 3</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 4</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 5</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 6</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 7</h1>
+      </div>
+      <div className='h-[350px] bg-gray-50 d-flex justify-content-center align-items-center p-6 rounded-lg shadow-md'>
+        <h1 className='text-lg font-semibold'>Camera 8</h1>
+      </div>
+     </div>
+    </div>
   )
 }
 
